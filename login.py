4254d184ab420a73c9c897095ec581cd3b54b4be
QuotaGamer <78985957+QuotaGamer@users.noreply.github.com>
2024-04-04 17:46:18 -0400

    recreation
    im remaking everything cuz wtf was i doing before like god..

--- login.py
+++ login.py
@@ -1,47 +1,14 @@
 from system.loginText import LOGINTEXT
 from system.opts import qlog as q
 from users.users import users
-import os
+import os, rsa
 
 if q == True:
     print("Quick Login (qlog) enabled.")
 print(LOGINTEXT)
-b=users.keys()
-a=str(b)
-a=a.replace("dict_keys([", "")
-a=a.replace("])", "")
-a=a.replace("'", "")
-a=a.replace(" ", "")
-c=a.split(",")
-d=[]
-for name in c:
-    d.append(users[name])
-for entry in d:
-    if entry == '':
-        d.remove(entry)
-a=a.replace(",", ", ")
-print(f"Valid users: {a}")
-#print(f"(Debug) Valid passwords: {d}")
-username = input("Username> ")
-if username == "guest":
-    os.system("python mss.py guest")
-    exit()
-elif username == "qlog":
-    if q == True:
-        os.system("python mss.py qlog")
-    else:
-        print("Nuh uh.")
-    exit()
-else:
-    userlist=a.split(", ")
-    print(userlist)
-    if username in userlist:
-        print("Yes.")
-        if username == "quota":
-            passwd=input("Password> ")
-            if passwd == users[username]:
-                os.system(f"python mss.py {username}")
-                exit()
-    else:
-        print(username)
-        print("Invalid user.")
+a:list=list(users.keys())
+d:str=""
+a.reverse()
+for i in a: d:str=f"{i}, {d}"
+d:str=d.removesuffix(", ")
+print(d)
